979257c476faf727a86f18d4300f7b8c966fbb81
jymknk1267 <149265648+jymknk1267@users.noreply.github.com>
2026-07-06 16:45:09 +0400

    Implement query script with OpenAI and Chroma
    This script loads environment variables, sets up a CLI for querying a database using embeddings, and formats a prompt for the OpenAI model to generate a response based on the query.

---
+++ query.py
@@ -0,0 +1,58 @@
+import argparse
+from dotenv import load_dotenv
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+CHROMA_PATH = "chroma"
+
+PROMPT_TEMPLATE = """
+Answer the question based only on the following context:
+
+{context}
+
+---
+
+Answer the question based on the above context: {question}
+"""
+
+
+def main():
+    # Create CLI.
+    parser = argparse.ArgumentParser()
+    parser.add_argument("query_text", type=str, help="The query text.")
+    args = parser.parse_args()
+    query_text = args.query_text
+
+    # Prepare the DB.
+    embedding_function = OpenAIEmbeddings(model="text-embedding-3-small")
+    db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
+
+    # Search the DB.
+    results = db.similarity_search_with_relevance_scores(query_text, k=3)
+    for doc, score in results:
+        print(f"{score:.4f} — {doc.page_content[:80]}")
+    if len(results) == 0:
+        print(f"Unable to find matching results.")
+        return
+
+    context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+    prompt = prompt_template.format(context=context_text, question=query_text)
+    print(prompt)
+
+    model = ChatOpenAI(model="gpt-4.1-nano")
+    response = model.invoke(prompt)
+    response_text = response.content
+
+    sources = [doc.metadata.get("source", None) for doc, _score in results]
+    formatted_response = f"Response: {response_text}\nSources: {sources}"
+    print(formatted_response)
+
+
+if __name__ == "__main__":
+    main()
